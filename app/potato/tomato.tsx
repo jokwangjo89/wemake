cc84413fa20672c99444ad3e81b604bff6b688bd
jokwangjo89 <samgugjojo@gmail.com>
2025-07-14 20:31:47 +0900

    Shadcn

---
+++ app/potato/tomato.tsx
@@ -0,0 +1,8 @@
+export default function AboutUs() {
+    return <div>About Us</div>
+}
+
+export const links = () => [
+    { rel: "stylesheet", href: "postato.com" },
+];
+export const meta = () => [{ title: "Tomato" }, { name: "description", content: "Tomato" }];
